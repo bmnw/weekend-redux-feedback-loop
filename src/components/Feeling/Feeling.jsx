84612a8d191a575e4156c2c380dc3fcd63662bac
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-09-08 15:00:56 -0500

    added redux store code to index.js including two reducers, one for feeling and one for content rating

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,8 +1,10 @@
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const Feeling = () => {
 
     const history = useHistory();
+    const dispatch = useDispatch();
 
     const toContent = () => {
         console.log('in toContent');
@@ -14,7 +16,7 @@ const Feeling = () => {
                 <h3>On a scale of 1-5, let us know how you're feeling today.</h3>
                 <h3>1: feeling pretty bad. 5: feeling awesome!</h3>
                 <form>
-                    <input type="number" />
+                    <input onChange={(event) => dispatch({type: 'append'})} type="number" />
                 </form>
                 <br />
                 <button onClick={toContent}>Next</button>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feelingRating = (state = 0, action) => {
+    console.log('feelingRating reducer', action);
+    return state;
+}
+
+const contentRating = (state = 0, action) => {
+    console.log('contentRating reducer', action);
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            feelingRating,
+            contentRating
+        }
+    )
+
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
